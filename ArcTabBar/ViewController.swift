32f321d6502c533b017add52917b237aaffdce19
Hay Van <haytranvan@hays-macbook-pro.local>
2018-10-09 18:02:23 +0700

    Initial Commit

---
+++ ArcTabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArcTabBar
+//
+//  Created by Hay Tran Van on 10/9/18.
+//  Copyright © 2018 Hay Tran Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
